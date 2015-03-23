794a68481c88ebf5f9284ccd6f0e80d4e6b6ffd1
Andy Rees <andyrees@users.noreply.github.com>
2015-03-23 22:32:05 +0000

    Update main.go

--- main.go
+++ main.go
@@ -33,59 +33,59 @@ import (
 	"strings"
 )
 
-type Media struct {
-	Streams []struct {
-		Codec_name      string  `json:"codec_name"`
-		Bit_rate        string  `json:"bit_rate"`
-		Duration_ts     float64 `json:"duration_ts"`
-		Codec_type      string  `json:"codec_type"`
-		Codec_time_base string  `json:"codec_time_base"`
-		Profile         string  `json:"Profile"`
-		Tags            struct {
-			Language          string `json:"language"`
-			Major_brand       string `json:"major_brand"`
-			Minor_version     string `json:"minor_version"`
-			Compatible_brands string `json:"compatible_brands"`
-			Handler_name      string `json:"handler_name"`
+type media struct {
+	streams []struct {
+		codecName     string  `json:"codec_name"`
+		bitRate       string  `json:"bit_rate"`
+		durationTs    float64 `json:"duration_ts"`
+		codecType     string  `json:"codec_type"`
+		codecTimeBase string  `json:"codec_time_base"`
+		profile       string  `json:"Profile"`
+		tags          struct {
+			language         string `json:"language"`
+			majorBrand       string `json:"major_brand"`
+			minorVersion     string `json:"minor_version"`
+			compatibleBrands string `json:"compatible_brands"`
+			handlerName      string `json:"handler_name"`
 		}
-		Duration             string  `json:"duration"`
-		Index                float64 `json:"index"`
-		Sample_aspect_ratio  string  `json:"sample_aspect_ratio"`
-		Codec_tag            string  `json:"codec_tag"`
-		Codec_tag_string     string  `json:"codec_tag_string"`
-		Start_pts            float64 `json:"start_pts"`
-		Sample_rate          string  `json:"sample_rate"`
-		Nb_frames            string  `json:"nb_frames"`
-		Avg_frame_rate       string  `json:"avg_frame_rate"`
-		Codec_long_name      string  `json:"codec_long_name"`
-		Display_aspect_ratio string  `json:"display_aspect_ratio"`
-		Level                float64 `json:"level"`
-		Has_b_frames         float64 `json:"has_b_frames"`
-		Pix_fmt              string  `json:"pix_fmt"`
-		Time_base            string  `json:"time_base"`
-		Height               float64 `json:"height"`
-		Bits_per_sample      float64 `json:"bits_per_sample"`
-		Width                float64 `json:"width"`
-		Sample_fmt           string  `json:"sample_fmt"`
-		Start_time           string  `json:"start_time"`
-		Channels             float64 `json:"channels"`
-		R_frame_rate         string  `json:"r_frame_rate"`
+		duration           string  `json:"duration"`
+		index              float64 `json:"index"`
+		sampleAspectRatio  string  `json:"sample_aspect_ratio"`
+		codecTag           string  `json:"codec_tag"`
+		codecTagString     string  `json:"codec_tag_string"`
+		startPts           float64 `json:"start_pts"`
+		sampleRate         string  `json:"sample_rate"`
+		nbFrames           string  `json:"nb_frames"`
+		avgFrameRate       string  `json:"avg_frame_rate"`
+		codecLongName      string  `json:"codec_long_name"`
+		displayAspectRatio string  `json:"display_aspect_ratio"`
+		level              float64 `json:"level"`
+		hasBFrames         float64 `json:"has_b_frames"`
+		pixFmt             string  `json:"pix_fmt"`
+		timeBase           string  `json:"time_base"`
+		height             float64 `json:"height"`
+		bitsPerSample      float64 `json:"bits_per_sample"`
+		width              float64 `json:"width"`
+		sampleFmt          string  `json:"sample_fmt"`
+		startTime          string  `json:"start_time"`
+		channels           float64 `json:"channels"`
+		rFrameRate         string  `json:"r_frame_rate"`
 	}
-	Format struct {
-		Format_name      string `json:"format_name"`
-		Format_long_name string `json:"ormat_long_name"`
-		Nb_streams       int    `json:"nb_streams"`
-		Duration         string `json:"duration"`
-		Start_time       string `json:"start_time"`
-		Size             string `json:"size"`
-		Filename         string `json:"filename"`
-		Bit_rate         string `json:"filename"`
-		Tags             struct {
-			Language          string `json:"language"`
-			Major_brand       string `json:"major_brand"`
-			Minor_version     string `json:"minor_version"`
-			Compatible_brands string `json:"compatible_brands"`
-			Handler_name      string `json:"handler_name"`
+	format struct {
+		formatName     string `json:"format_name"`
+		formatLongName string `json:"ormat_long_name"`
+		nbStreams      int    `json:"nb_streams"`
+		duration       string `json:"duration"`
+		startTime      string `json:"start_time"`
+		size           string `json:"size"`
+		filename       string `json:"filename"`
+		bitRate        string `json:"filename"`
+		tags           struct {
+			language         string `json:"language"`
+			majorBrand       string `json:"major_brand"`
+			minorVersion     string `json:"minor_version"`
+			compatibleBrands string `json:"compatible_brands"`
+			handlerName      string `json:"handler_name"`
 		}
 	}
 }
@@ -98,27 +98,27 @@ func fcheck(filepath string) error {
 	return nil
 }
 
-func get_data_from_ffprobe(filepath string) (string, error) {
-	ffprobe_path, lookerr := exec.LookPath("ffprobe")
+func getDataFromFfprobe(filepath string) (string, error) {
+	ffprobePath, lookerr := exec.LookPath("ffprobe")
 	if lookerr != nil {
 		return "", lookerr
 	}
 
-	json_result_bytes, err := exec.Command(ffprobe_path, "-show_format", "-show_streams", "-print_format", "json=c=1", filepath).Output()
+	jsonResultBytes, err := exec.Command(ffprobePath, "-show_format", "-show_streams", "-print_format", "json=c=1", filepath).Output()
 	if err != nil {
 		return "", err
 	}
 
-	return string(json_result_bytes), nil
+	return string(jsonResultBytes), nil
 }
 
-type MediaFileLoudness struct {
-	FileName                    string
-	PassedOrFailed              bool
-	Loudness                    string
-	RecommendedAdjustment       float64
-	RecommendedAdjustmentString string
-	Standard                    string
+type mediaFileLoudness struct {
+	fileName                    string
+	passedOrFailed              bool
+	loudness                    string
+	recommendedAdjustment       float64
+	recommendedAdjustmentString string
+	standard                    string
 }
 
 // This method takes an absolute file path and passes it to ffmpeg,
@@ -150,7 +150,7 @@ func getFfmpegReadings(filepath string) (string, error) {
 // takes an absolute file path, gets the Loudness readings
 // returns:
 //		passed bool, loudness string, adjustment float64, err error
-func check_file_loudness(filepath string, std bool) (bool, string, float64, error) {
+func checkFileLoudness(filepath string, std bool) (bool, string, float64, error) {
 	ffmpegResults, err := getFfmpegReadings(filepath)
 	if err != nil {
 		log.Fatalln(err)
@@ -162,27 +162,26 @@ func check_file_loudness(filepath string, std bool) (bool, string, float64, erro
 	result := regexpr.FindAllStringSubmatch(ffmpegResults, -1)
 
 	if len(result) > 0 && len(result[0]) > 0 {
-		integrated_loudness := result[len(result)-1][1]
-		integrated_loudness_float, err := strconv.ParseFloat(integrated_loudness, 64)
+		integratedLoudness := result[len(result)-1][1]
+		integratedLoudnessFloat, err := strconv.ParseFloat(integratedLoudness, 64)
 
 		if err != nil {
 			return false, "", 0.0, nil
 		}
 
 		if std {
-			if integrated_loudness_float >= -24 && integrated_loudness_float <= -22 {
-				return true, integrated_loudness, 0.0, nil
+			if integratedLoudnessFloat >= -24 && integratedLoudnessFloat <= -22 {
+				return true, integratedLoudness, 0.0, nil
 			} else {
-				return false, integrated_loudness, (-23 - integrated_loudness_float), nil
+				return false, integratedLoudness, (-23 - integratedLoudnessFloat), nil
 			}
 		} else {
-			if integrated_loudness_float >= -26 && integrated_loudness_float <= -22 {
-				return true, integrated_loudness, 0.0, nil
+			if integratedLoudnessFloat >= -26 && integratedLoudnessFloat <= -22 {
+				return true, integratedLoudness, 0.0, nil
 			} else {
-				return false, integrated_loudness, (-24 - integrated_loudness_float), nil
+				return false, integratedLoudness, (-24 - integratedLoudnessFloat), nil
 			}
 		}
-		return false, integrated_loudness, 0.0, nil
 	}
 	return false, "", 0.0, errors.New("REGEX PARSE ERROR")
 
@@ -206,38 +205,38 @@ func main() {
 		log.Fatalf("%v\n", err.Error())
 	}
 
-	source_settings, err := get_data_from_ffprobe(*checkFile)
+	sourceSettings, err := getDataFromFfprobe(*checkFile)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	m := new(Media)
-	json.Unmarshal([]byte(source_settings), &m)
+	m := new(media)
+	json.Unmarshal([]byte(sourceSettings), &m)
 
 	var (
 		acodec          string
-		AudioBitRate    string
-		AudioChannels   float64
-		AudioSampleRate string
+		audioBitRate    string
+		audioChannels   float64
+		audioSampleRate string
 	)
 
-	for _, stream := range m.Streams {
-		if stream.Codec_type == "audio" {
+	for _, stream := range m.streams {
+		if stream.codecType == "audio" {
 			// log.Printf("AUDIO CODEC: %+v\n", stream.Codec_name)
-			acodec = stream.Codec_name
+			acodec = stream.codecName
 
 			// log.Printf("AUDIO BITRATE: %+v\n", stream.Bit_rate)
-			AudioBitRate = stream.Bit_rate
+			audioBitRate = stream.bitRate
 
 			// log.Printf("AUDIO SAMPLE RATE: %+v\n", stream.Sample_rate)
-			AudioSampleRate = stream.Sample_rate
+			audioSampleRate = stream.sampleRate
 
 			// log.Printf("AUDIO Channels: %+v\n", stream.Channels)
-			AudioChannels = stream.Channels
+			audioChannels = stream.channels
 		}
 	}
 
-	passed, loudness, adjustment, err := check_file_loudness(*checkFile, *loudnessStd)
+	passed, loudness, adjustment, err := checkFileLoudness(*checkFile, *loudnessStd)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -250,13 +249,13 @@ func main() {
 		std = "ATSC A/85 RP  = -24 LKFS +/- 2, True Peak -2dB maximum"
 	}
 
-	mf := MediaFileLoudness{}
-	mf.FileName = path.Base(*checkFile)
-	mf.PassedOrFailed = passed
-	mf.Loudness = loudness
-	mf.RecommendedAdjustment = adjustment
-	mf.RecommendedAdjustmentString = fmt.Sprintf("%.1fdB", adjustment)
-	mf.Standard = std
+	mf := mediaFileLoudness{}
+	mf.fileName = path.Base(*checkFile)
+	mf.passedOrFailed = passed
+	mf.loudness = loudness
+	mf.recommendedAdjustment = adjustment
+	mf.recommendedAdjustmentString = fmt.Sprintf("%.1fdB", adjustment)
+	mf.standard = std
 
 	fname := path.Base(*checkFile)
 	fdir := path.Dir(*checkFile)
@@ -267,22 +266,22 @@ func main() {
 	switch strings.ToLower(*outputFormat) {
 	case "json":
 		// create json object
-		json_obj, err := json.MarshalIndent(mf, "", " ")
+		jsonObj, err := json.MarshalIndent(mf, "", " ")
 		if err != nil {
 			log.Fatalln(err)
 		}
-		os.Stdout.Write(json_obj)
+		os.Stdout.Write(jsonObj)
 		fmt.Println("")
 	case "xml":
 		// create xml object
-		xml_obj, err := xml.MarshalIndent(mf, "", " ")
+		xmlObj, err := xml.MarshalIndent(mf, "", " ")
 		if err != nil {
 			log.Fatalln(err)
 		}
-		os.Stdout.Write(xml_obj)
+		os.Stdout.Write(xmlObj)
 		fmt.Println("")
 	case "simple":
-		fmt.Fprintf(os.Stdout, "%s\nLoudness: %s\nAdjustment: %s\nPassed=%t\n", mf.FileName, mf.Loudness, mf.RecommendedAdjustmentString, mf.PassedOrFailed)
+		fmt.Fprintf(os.Stdout, "%s\nLoudness: %s\nAdjustment: %s\nPassed=%t\n", mf.fileName, mf.loudness, mf.recommendedAdjustmentString, mf.passedOrFailed)
 	default:
 		fmt.Println("File checked to loudness standard: ", std)
 		if passed {
@@ -300,12 +299,12 @@ func main() {
 
 	if !passed {
 		if *autoFix {
-			ffmpeg_path, err := exec.LookPath("ffmpeg")
+			ffmpegPath, err := exec.LookPath("ffmpeg")
 			if err != nil {
 				log.Fatalf("ffmpeg Error: %v", err.Error())
 			}
 			runCmd := exec.Command(
-				ffmpeg_path,
+				ffmpegPath,
 				"-threads",
 				"auto",
 				"-i",
@@ -315,11 +314,11 @@ func main() {
 				"-acodec",
 				fmt.Sprintf("%s", acodec),
 				"-b:a",
-				fmt.Sprintf("%v", AudioBitRate),
+				fmt.Sprintf("%v", audioBitRate),
 				"-ac",
-				fmt.Sprintf("%v", AudioChannels),
+				fmt.Sprintf("%v", audioChannels),
 				"-ar",
-				fmt.Sprintf("%v", AudioSampleRate),
+				fmt.Sprintf("%v", audioSampleRate),
 				"-strict",
 				"experimental",
 				"-q:v",
@@ -327,7 +326,7 @@ func main() {
 				"-q:a",
 				"1",
 				"-filter_complex",
-				fmt.Sprintf("volume=volume=%s", mf.RecommendedAdjustmentString),
+				fmt.Sprintf("volume=volume=%s", mf.recommendedAdjustmentString),
 				"-y",
 				fmt.Sprintf("%s", outfile),
 			)
